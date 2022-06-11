e7e195024db17b5d5070f4f6e3e5412c79abac9c
angulo66 <helderl.angulo@gmail.com>
2022-06-11 15:11:57 -0600

    added algorithm to fetch usd price via 3 mediums

--- pages/api/hello2.js
+++ pages/api/hello2.js
@@ -42,7 +42,7 @@ const test2 = {
     tokenId: "ethereum",
     tokenInAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
     tokenOutAddress: '0x11c1a6b3ed6bb362954b29d3183cfa97a0c806aa',
-    amount: 5,
+    amount: .5,
     poolIds: [
         "0xbb2b8038a1640196fbe3e38816f3e67cba72d940", // ETH>WBTC
         "0x0731ee4cf7376a1bd5a78199ac9bedc8213def24", // WBTC>WPE
@@ -61,8 +61,47 @@ const test2 = {
     ]
 }
 
+const test3 = {
+    name: "DAI>ETH",
+    tokenId: "dai",
+    tokenInAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
+    tokenOutAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    amount: 200,
+    poolIds: [
+        "0xfc0dd985f6de9c2322ebd97c3422b0857c4d78c7",
+        "0x648450d9c30b73e2229303026107a1f7eb639f6c"
+    ],
+    tokenPaths: [
+        "0x6b175474e89094c44da98b954eedeac495271d0f",
+        "0x1337def16f9b486faed0293eb623dc8395dfe46a",
+        "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    ],
+    tokenNames: [
+        "dai",
+    ]
+}
+
+const test4 = {
+    name: "WPE>ETH",
+    tokenId: "ethereum",
+    tokenOutAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    tokenInAddress: '0xd075e95423c5c4ba1e122cae0f4cdfa19b82881b',
+    amount: 2,
+    poolIds: [
+        "0x75f89ffbe5c25161cbc7e97c988c9f391eaefaf9" // ETH>WPE
+    ],
+    tokenPaths: [
+        "0xd075e95423c5c4ba1e122cae0f4cdfa19b82881b", // WPE
+        "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    ],
+    tokenNames: [
+        "opes-wrapped-pe",
+    ]
+}
+
+
 export default async function handler(req, res) {
-    const test = test0;
+    const test = test4;
     let t1 = performance.now();
     console.warn("Swap for pairs with hops test:::");
     test.tokenPaths.slice(1);
--- pages/libs/queries.js
+++ pages/libs/queries.js
@@ -72,6 +72,65 @@ export const pairsByIdQuery = (id) => {
       reserve0
       reserve1
       totalSupply
+      reserveETH
+      reserveUSD
+      token0Price
+      token1Price
     }
 }`;
 };
+
+export const pairsWithETHQuery = (id) => {
+  return `{
+    result1: pairs(first: 1, where: {
+      token0: "${id}"
+      , token1: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    }
+    ) {
+      id
+    token0 {
+        id
+        decimals
+        symbol
+      }
+    token1 {
+        id
+        decimals
+        symbol
+      }
+      reserve0
+      reserve1
+      totalSupply
+      reserveETH
+      reserveUSD
+      volumeUSD
+      token0Price
+      token1Price
+    },
+    result2: pairs(first: 1, where: {
+      token0: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+      token1: "${id}"
+    },
+    ) {
+      id
+    token0 {
+        id
+        decimals
+        symbol
+      }
+    token1 {
+        id
+        decimals
+        symbol
+      }
+      reserve0
+      reserve1
+      totalSupply
+      reserveETH
+      reserveUSD
+      volumeUSD
+      token0Price
+      token1Price
+    }
+  }`;
+}
